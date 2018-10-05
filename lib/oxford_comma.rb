d39e5ca50675a5c93a3b9e9e2f7578d3c82d96e0
vibhu mahendru <vibhumahendru@gmail.com>
2018-10-05 01:18:30 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,6 @@
 def oxford_comma(array)
-  if array.length == 2
-  array[-2] << " and "
-  array.join
-  elsif array.length == 1
-  	array.join
-  elsif array.length > 2
-    array[-1].prepend "and "
-	array.join(", ")
-  end
+if array.length == 1 
+  x =  array.to_s()
 end
-
+puts x
+end
