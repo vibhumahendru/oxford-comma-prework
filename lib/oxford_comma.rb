4f1aefbc89a5f28cd474171cbc1f3c475290b778
vibhu mahendru <vibhumahendru@gmail.com>
2018-10-05 01:32:14 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,11 +4,7 @@ array.join
 elsif array.length == 2
 array.join(" and ")
 elsif array.length >=3
-newAr = []
-for x in 0...(array.length-1)
-newAr.push(array[x])
-end
-newAr.join(", ")
-newAr + ", and " + array[-1]
+
+
 end
 end
