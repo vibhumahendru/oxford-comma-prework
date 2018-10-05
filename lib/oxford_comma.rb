71b1cc36509ad040d9d757dc91f6706632b581d8
vibhu mahendru <vibhumahendru@gmail.com>
2018-10-05 01:27:33 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,10 @@ array.join
 elsif array.length == 2
 array.join(" and ")
 elsif array.length >=3
-array.join
+newAr = []
+for x in 0...(array.length-1)
+newAr.push(array[x])
 end
+newAr.join(", ")
+newAr + " and " + array[-1]
 end
