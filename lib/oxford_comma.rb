8f8913a9c9a67d5c0f75b710fc2c5c1aeefa22e5
vibhu mahendru <vibhumahendru@gmail.com>
2018-10-05 01:18:00 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,13 @@
 def oxford_comma(array)
-if array.length == 1 
-  x =  array.to_s()
+  if array.length == 2
+  array[-2] << " and "
+  array.join
+  elsif array.length == 1
+  	array.join
+  elsif array.length > 2
+    array[-1].prepend "and "
+	array.join(", ")
+  end
 end
-puts x
-end
+
+oxford_comma(array)
