e090252a20e50a8e8bcfb2571d5a459a411ef496
vibhu mahendru <vibhumahendru@gmail.com>
2018-10-05 01:38:26 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
+if array.length == 1 
+array.join
+elsif array.length == 2
+array.join(" and ")
+elsif array.length >=3
+newAr = array[-1]
+array.pop
+array.push("and")
+array.join(", ") +  " #{newAr}"
 
+end
 end
